4a1a979fc22ab712701f2556ee925066771c13d3
Vasavi Chindalur <vasavi.chindalur@branch.io>
2020-07-06 15:29:45 -0700

    Add brand and model parsing

--- src/test/java/ua_parser/ParserTest.java
+++ src/test/java/ua_parser/ParserTest.java
@@ -78,16 +78,22 @@ public class ParserTest {
   public void testParseAll() {
     String agentString1 = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; fr; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5,gzip(gfe),gzip(gfe)";
     String agentString2 = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3";
+    String agentString3 = "Mozilla/5.0 (Linux; Android 10; ONEPLUS A6010) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36";
 
     Client expected1 = new Client(new UserAgent("Firefox", "3", "5", "5"),
                                   new OS("Mac OS X", "10", "4", null, null),
-                                  new Device("Other"));
+                                  new Device("Mac", "Apple", "Mac"));
     Client expected2 = new Client(new UserAgent("Mobile Safari", "5", "1", null),
                                   new OS("iOS", "5", "1", "1", null),
-                                  new Device("iPhone"));
+                                  new Device("iPhone","Apple","iPhone"));
+    Client expected3 = new Client(new UserAgent("Chrome Mobile", "83", "0", "4103"),
+                                  new OS("Android", "10", null, null, null),
+                                  new Device("OnePlus ONEPLUS A6010",  "OnePlus", "ONEPLUS A6010"));
+
 
     assertThat(parser.parse(agentString1), is(expected1));
     assertThat(parser.parse(agentString2), is(expected2));
+    assertThat(parser.parse(agentString3), is(expected3));
   }
 
   @Test
